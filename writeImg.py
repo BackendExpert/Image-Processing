74efe0d1541388e7b45b07f006bbdf0be2cff9b5
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-03-17 19:01:06 +0530

    Update writeImg.py

--- writeImg.py
+++ writeImg.py
@@ -14,4 +14,4 @@ image = cv2.imread(img_path)
 newfolder_path = os.path.join(folder_path, os.path.basename(img_path))
 cv2.imwrite(newfolder_path, image)
 
-print("Image Added folder", folder_path)
+print("Image Added folder", folder_path)    
